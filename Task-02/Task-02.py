bba4859670c8358a7676deb1a1ee0793afc23fc6
ProgmanZ <dumphill@gmail.com>
2022-10-17 23:48:21 +0300

    Prog before check on directory

--- Task-02/Task-02.py
+++ Task-02/Task-02.py
@@ -1,14 +1,20 @@
 # 23.2 Обработка исключений: операторы try except
 
 import random
+import os
 
 try:
 
-    ages_file = open('ages.txt', 'x', encoding='utf-8')
-    [ages_file.write(str(random.randint(10, 99)) + '\n') for age in range(10)]
-    ages_file.close()
+    # Этот блок запрашивает имя файла и записывает в него случайные 10 значений возраста
+    # user_file = input('Введите имя файла куда будут записаны значения возраста: ')
+    # ages_file = open(f'{user_file}.txt', 'x', encoding='utf-8')
+    # [ages_file.write(str(random.randint(10, 99)) + '\n') for age in range(10)]
+    # ages_file.close()
 
-    ages_file = open('ages.txt', 'r', encoding='utf-8')
+    user_file = input('Введите имя файла: ')
+    user_path = os.path.abspath(user_file)
+    print(user_path)
+    ages_file = open(''.join([user_path, '.txt' if not user_file.endswith('.txt') else '']), 'r', encoding='utf-8')
     ages_db = ages_file.read()
     ages_file.close()
 
@@ -19,16 +25,31 @@ try:
     ages_db = ages_db.split()
     names_db = names_db.strip().split(', ')
 
-    result_file = open('result.txt', 'w', encoding='utf-8')
-    for age in ages_db:
-        result_file.write(f'{str(age)} - {random.choice(names_db)}\n')
-    result_file.close()
+    try:
+        result_file = open('result.txt', 'x', encoding='utf-8')
+    except FileExistsError:
+        print('Ой. Файл уже существует')
+        user_answer = ''
+        while user_answer != 'y' and user_answer != 'n':
+            user_answer = input('Хотите переписать файл?').lower()
+        if user_answer == 'y':
+            result_file = open('result.txt', 'w', encoding='utf-8')
 
-except FileExistsError as exc:
-    print(f'Файл ages.txt уже существует.')
+            for age in ages_db:
+                try:
+                    result_file.write(f'{str(int(age))} - {random.choice(names_db)}\n')
+                except ValueError as exc:
+                    print(f'Нашлась ошибка в файле {user_file if user_file.endswith(".txt") else f"{user_file}.txt"}')
+                    print(f'Исключение: {exc}')
+                    print(f'Тип исключения: {type(exc)}')
+            result_file.close()
+        else:
+            print('Никаких изменений не было внесено.')
+
+
+except FileNotFoundError as exc:
+    print('Такого файла не существует')
     print(f'Исключение: {exc}')
     print(f'Тип исключения: {type(exc)}')
-
-
 
 
